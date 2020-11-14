468500b3b01b448a38426a3a96a7f7d43a551d2d
Ben Hong <ben@bencodezen.io>
2020-11-14 16:15:05 -0500

    feat: add basic styling for headers and page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <main class="main-wrapper">
+    <h1 class="page-title">Vue Your Todo</h1>
+    <p class="subtitle">A todo app powered by Vue.js 3</p>
+    <input type="text" />
+    <button>+ Add</button>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
+
+<style>
+html {
+  background-color: #fbfbfb;
+}
+
+.main-wrapper {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.page-title {
+  font-family: 'DM Serif Display', serif;
+  font-size: 44px;
+  letter-spacing: 1.84px;
+  color: #2d2d2d;
+  margin-top: 104px;
+  margin-bottom: 0;
+}
+
+.subtitle {
+  font-size: 1rem;
+  font-weight: bold;
+  color: #6b6b6b;
+  margin-top: 0;
+}
+</style>
