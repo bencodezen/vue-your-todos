bf66d2303902545557260c66574c3c2cd8b8687c
Ben Hong <ben@bencodezen.io>
2020-11-14 16:26:41 -0500

    feat: add new task input with updated typography

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <main class="main-wrapper">
     <h1 class="page-title">Vue Your Todo</h1>
     <p class="subtitle">A todo app powered by Vue.js 3</p>
-    <input type="text" />
-    <button>+ Add</button>
+    <div class="new-task-wrapper">
+      <input
+        type="text"
+        placeholder="Type a new todo item"
+        class="new-task-input"
+      />
+      <button class="new-task-button">+ Add</button>
+    </div>
   </main>
 </template>
 
@@ -16,6 +22,32 @@ export default {
 <style>
 html {
   background-color: #fbfbfb;
+}
+
+.new-task-wrapper {
+  display: flex;
+}
+
+.new-task-input {
+  padding: 16px;
+  font-weight: 600;
+  color: #959595;
+  flex: 1;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  border: 1px solid #f6f6f6;
+  box-shadow: 2px 2px 8px 0 #c0c0c0;
+  letter-spacing: 1px;
+}
+
+.new-task-button {
+  background-color: #0631f8;
+  color: #fff;
+  padding: 18px 24px;
+  font-weight: 900;
+  border: 0;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
 }
 
 .main-wrapper {
@@ -37,5 +69,6 @@ html {
   font-weight: bold;
   color: #6b6b6b;
   margin-top: 0;
+  letter-spacing: 0.67px;
 }
 </style>
